a6be943059480d5f3ddd92b6687c28c665506237
Behemyth <asher.norland@gmail.com>
2016-05-13 18:33:15 -0400

    crapton

---
+++ GameJam2016/Object.cpp
@@ -0,0 +1,191 @@
+#include "Object.h"
+#include "ObjLoader.h"
+
+Object::Object()
+{
+	isStatic = false;
+	isGhost = false;
+	position = glm::mat4();
+	fragmentName = "fragment-shader[basic].txt";
+	displacementName = "gray.png";
+	vertexName = "vertex-shader[basic].txt";
+	textureName = "dirt.jpg";
+	normalName = "black.png";
+}
+
+
+Object::~Object()
+{
+}
+
+void Object::ExtractFromFile(const char* name){
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string err = tinyobj::LoadObj(shapes, materials, name, NULL);
+
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+	}
+
+	uint overallSize = 0;
+	uint faceOverallSize = 0;
+	for (uint i = 0; i < shapes.size(); i++){
+		overallSize += shapes[i].mesh.positions.size();
+		faceOverallSize += shapes[i].mesh.indices.size();
+	}
+
+
+
+	uint overallOffset = 0;
+	uint faceOffset = 0;
+
+	for (uint i = 0; i < shapes.size(); i++){
+		for (uint v = 0; v < overallSize / 3; v++){
+			vertices.push_back({
+				glm::vec3(shapes[i].mesh.positions[3 * v + 0], shapes[i].mesh.positions[3 * v + 1], shapes[i].mesh.positions[3 * v + 2]),
+				glm::vec2(shapes[i].mesh.texcoords[2 * v + 0], shapes[i].mesh.texcoords[2 * v + 1]),
+				glm::vec3(shapes[i].mesh.normals[3 * v + 0], shapes[i].mesh.normals[3 * v + 1], shapes[i].mesh.normals[3 * v + 2]) });
+		}
+		overallOffset += shapes[i].mesh.positions.size();
+
+		for (uint f = 0; f < faceOverallSize / 3; f++){
+			indices.push_back({
+				glm::uvec3(shapes[i].mesh.indices[3 * f + 0], shapes[i].mesh.indices[3 * f + 1], shapes[i].mesh.indices[3 * f + 2]) });
+				//shapes[i].mesh.material_ids[f] });
+		}
+		faceOffset += shapes[i].mesh.indices.size();
+	}
+}
+
+
+void Object::Draw(Camera& camera)
+{
+	if (shader == NULL){
+		std::cout << "Forgot to call Load()" << std::endl;
+	}
+		shader->use();
+		glBindVertexArray(vao);
+		shader->setUniform(cameraUniform, camera.matrix());
+		shader->setUniform(texUniform, 0);
+		shader->setUniform(disUniform, 1);
+		shader->setUniform(normUniform, 2);
+		shader->setUniform(posNormUniform, glm::inverse(glm::transpose(position)));
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture->object());
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, displacement->object());
+
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, normal->object());
+
+		shader->setUniform(posUniform, position);
+		glDrawElements(GL_TRIANGLES, (indices.size() * 3), GL_UNSIGNED_INT, (GLvoid*)0);
+		glBindVertexArray(0);
+		shader->stopUsing();
+}
+
+void Object::NormalizeScale(glm::vec3 scale){
+	for (int i = 0; i < GetVertices().size();i++){
+		GetVertices()[i].position *=  scale;
+	}
+}
+Bitmap Object::LoadBmp(const char* filename) {
+	Bitmap bmp = Bitmap::bitmapFromFile(filename);
+	bmp.flipVertically();
+	return bmp;
+}
+Texture* Object::LoadTexture(Bitmap bmp){
+	return new Texture
+		(bmp);
+}
+
+void Object::Load(){
+
+	if (!isStatic){
+		btTransform trans;
+		trans.setFromOpenGLMatrix(glm::value_ptr(position));
+		btDefaultMotionState* motState =
+			new btDefaultMotionState(trans);
+		btScalar mass = 1;
+		btVector3 fallInertia(0, 0, 0);
+		shape->calculateLocalInertia(mass, fallInertia);
+		btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, motState, shape, fallInertia);
+		rigidBody = new btRigidBody(fallRigidBodyCI);
+		rigidBody->setFriction(1.35f);
+		if (isGhost){
+			rigidBody->setCollisionFlags(rigidBody->getCollisionFlags() | rigidBody->CF_NO_CONTACT_RESPONSE);
+		}
+		world->addRigidBody(rigidBody);
+	}
+	else{
+		btTransform trans;
+		trans.setFromOpenGLMatrix(glm::value_ptr(position));
+		btDefaultMotionState* motState =
+			new btDefaultMotionState(trans);
+		btRigidBody::btRigidBodyConstructionInfo
+			groundRigidBodyCI(0, motState, shape, btVector3(0, 0, 0));
+		rigidBody = new btRigidBody(groundRigidBodyCI);
+		rigidBody->setFriction(1.35f);
+		if (isGhost){
+			rigidBody->setCollisionFlags(rigidBody->getCollisionFlags() | rigidBody->CF_NO_CONTACT_RESPONSE);
+		}
+		world->addRigidBody(rigidBody);
+	}
+
+	shader = LoadShaders(vertexName, "geometry-shader[basic].txt", fragmentName);
+	cameraUniform = shader->uniform("camera");
+	posNormUniform = shader->uniform("normalPos");
+	texUniform = shader->uniform("tex");
+	disUniform = shader->uniform("dis");
+	normUniform = shader->uniform("norm");
+	posUniform = shader->uniform("position");
+	normal = LoadTexture(LoadBmp(normalName));
+	texture = LoadTexture(LoadBmp(textureName));
+	displacement = LoadTexture(LoadBmp(displacementName));
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*vertices.size(), &vertices.front(), GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(shader->attrib("vert_VS_in"));
+	glVertexAttribPointer(shader->attrib("vert_VS_in"), 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), NULL);
+	glEnableVertexAttribArray(shader->attrib("frag_VS_in"));
+	glVertexAttribPointer(shader->attrib("frag_VS_in"), 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (const GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(shader->attrib("normal_VS_in"));
+	glVertexAttribPointer(shader->attrib("normal_VS_in"), 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (const GLvoid*)(5 * sizeof(GLfloat)));
+
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Index)*indices.size(), &indices.front(), GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
+}
+
+void Object::Update(double dt){
+	
+}
+void Object::UpdatePosition(){
+	btTransform trans;
+	rigidBody->getMotionState()->getWorldTransform(trans);
+	trans.getOpenGLMatrix(glm::value_ptr(position));
+}
+std::vector<Index>& Object::GetIndices(){
+	return indices;
+}
+std::vector<Vertex>& Object::GetVertices(){
+	return vertices;
+}
+btRigidBody* Object::GetRigidBody(){
+	return rigidBody;
+}
+glm::vec3 Object::GetPosition(){
+	return glm::vec3(position[3]);
+}
+
+glm::mat4 Object::GetMatrix(){
+	return position;
+}
---
+++ GameJam2016/Object.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "BasicIncludes.h"
+#include "ShaderSupport.h"
+#include "Camera.h"
+#include "Texture.h"
+
+//This class represents anything that is drawable onscreen. 
+class Object
+{
+public:
+	Object();
+	~Object();
+
+	virtual void Draw(Camera&);
+	virtual void Load();
+	virtual void Update(double );
+	btRigidBody* GetRigidBody();
+	glm::vec3 GetPosition();
+	glm::mat4 GetMatrix();
+
+	virtual void UpdatePosition();
+	void ExtractFromFile(const char* name);
+
+	std::vector<Index>& GetIndices();
+	std::vector<Vertex>& GetVertices();
+
+protected:
+	bool isStatic;
+	bool isGhost;
+	btDiscreteDynamicsWorld* world;
+	btCollisionShape* shape;
+	btRigidBody* rigidBody;
+	glm::mat4 position;
+	glm::mat4 positionNormal;
+	char* textureName;
+	char* displacementName;
+	char* normalName;
+	char* fragmentName;
+	char* vertexName;
+
+	void NormalizeScale(glm::vec3);
+
+private:
+	std::vector<Vertex> vertices;
+	std::vector<Index> indices;
+
+	Texture* texture;
+	Texture* displacement;
+	Texture* normal;
+
+	Bitmap LoadBmp(const char*);
+	Texture* LoadTexture(Bitmap);
+
+	//a matrix defining rotation, translation, and scaling of the object
+	//only use if the vertices are object space and not world space
+	
+	GLuint cameraUniform;
+	GLuint posUniform;
+	GLuint texUniform;
+	GLuint disUniform;
+	GLuint normUniform;
+	GLuint posNormUniform;
+
+	shading::ShaderSupport* shader;
+	GLuint vao;
+	GLuint vbo;
+	GLuint ibo;
+
+	
+};
+
